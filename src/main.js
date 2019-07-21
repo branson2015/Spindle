8d944dd0b859b2bac59a5ee62fd50acbd2451d80
Siracle <branson2015@gmail.com>
2019-07-21 11:41:56 -0500

    removed function in mapping, added callbacks in setter, standardized linking with Spindle.link object/

--- src/main.js
+++ src/main.js
@@ -3,37 +3,48 @@ import * as Aux from './Auxillary.js'
 
 //TODO: make get function that returns which element(s) the object is bound to
 //make different ways of binding function (auto create object, make window the htmlScope, etc)
+//add callback onchange functions
+
+function LINK(element, type, callback){
+    this['element'] = element;
+    this['type'] = type;
+    this['callback'] = callback;
+}
+
+export function link(element, type, callback){
+    if(typeof element === 'object') return new LINK(element['element'], element['type'], element['callback']);
+    return new LINK(element, type, callback)
+}
 
 export function Bind(object, htmlScope, mapping){
-    if(typeof htmlScope === 'string')   htmlScope = document.querySelector(htmlScope);  //change to querySelectorAll for multibinding
-    if(!isElement(htmlScope))   throw 'bad element identifier';
+    if(typeof htmlScope === 'string')   htmlScope = document.querySelectorAll(htmlScope);
+    if(htmlScope.constructor === Array){
+        if(htmlScope.length > 1){
+            for(scope in htmlScope)    Bind(object, scope, mapping);    return;
+        }else
+            htmlScope = htmlScope[0];
+    }else if(!isElement(htmlScope))   throw 'bad element identifier';
     
-    //for every mapping
     for(var key in mapping){
-        //reset obj to original object on each loop
         var obj = object;
         
         var identifiers = mapping[key];
-        if(typeof identifiers !== 'object' || isElement(identifiers))   //keep objects in object form, put into recursion (also includes arrays)
+        if(typeof identifiers !== 'object' || identifiers.constructor === LINK || isElement(identifiers))   //keep objects in object form, put into recursion (also includes arrays)
             identifiers = [identifiers];
 
         if(typeof obj[key] === 'object'){
-            Bind(obj[key], htmlScope, identifiers);
-            continue;
+            Bind(obj[key], htmlScope, identifiers); continue;
         }
         
         //shortcuts (using weird arrays to hack in references)
         key = key.split('.');
         var curr = [key.shift()];
-        for(; key.length > 0; curr = [key.shift()]){//length - 1 because if we go to length then obj[i] will be primitive and will not change obj[i], not a reference
-            var index = getIndex(curr);
-            obj = (index !== -1) ? obj[curr[0]][index] : obj[curr[0]];
-        }
+        for(; key.length > 0; curr = [key.shift()])//length - 1 because if we go to length then obj[i] will be primitive and will not change obj[i], not a reference
+            var index = getIndex(curr), obj = (index !== -1) ? obj[curr[0]][index] : obj[curr[0]];
+
         var index = getIndex(curr);
-        if(index !== -1){
-            obj = obj[curr[0]];
-            curr[0] = index;
-        }
+        if(index !== -1)
+            obj = obj[curr[0]], curr[0] = index;
         
         bindobj(obj, curr[0], elementTypeBundle(htmlScope, identifiers));
     }
@@ -56,35 +67,46 @@ function bindobj(obj, key, els){
 
     //set events to fire here onchange
     Object.defineProperty(obj, key, {
-        get: ()=>{ return obj[key] = els[0]['e'][els[0]['t']]; },
-        set: (v)=>{ els.forEach((el)=>{el['e'][el['t']] = v;}) }
+        get: ()=>{ return obj[key] = els[0]['e'][els[0]['t']];  },
+        set: (v)=>{ els.forEach((el)=>{el['e'][el['t']] = v; if(el['c']) el['c'](v)}); },
     });
 }
 
 function elementTypeBundle(htmlscope, identifiers){
     var els = [];
     identifiers.forEach((id)=>{
-        //TODO: put shortcuts for defining which type IN STRING to modify
-        //TODO: implement objects: {'element': 'whatever', 'type': 'whatever'}
-        if(isElement(id))
-            return els.push({'e': id, 't': Att.getDefaultAttribute(id.tagName)});
-
-        id = (function translate(id){ return (typeof id === 'function') ?  translate(id()) : id; })(id);
-        els = els.concat(getElements(htmlscope, id));
+        var el, type, callback;
+        
+        if(id.constructor === LINK)
+            el = bundle(htmlscope, id['element'], type = id['type'], callback = id['callback']);
+        else if(typeof id === 'string' || isElement(id))
+            el = bundle(htmlscope, id, type);
+        else
+            throw 'error, object not in right form'
+        els = els.concat(el);
     });
     return els;
 }
 
-function getElements(scope, query){
-    var bundle = [];
-    scope.querySelectorAll(query).forEach((element)=>{
-        bundle.push({'e' : element, 't' : Att.getDefaultAttribute(element.tagName)});
-    });
-    return bundle;
+//TODO: maybe make scope element queriable as well?
+function bundle(scope, query, type, callback){
+    var group;
+    if(typeof query === 'string')
+        group = scope.querySelectorAll(query);
+    else if(isElement(query))
+        group = [query];
+    else
+        throw 'error'
+
+    var b = [];
+    for(var i = 0; i < group.length; ++i)
+        b.push({'e' : group[i], 't' : type || Att.getDefaultAttribute(group[i].tagName), 'c': callback});
+    return b;
 }
 
 function getIndex(str, i = 0){
-    var start = str[i].indexOf('[') + 1, dist = str[i].indexOf(']') - start;
+    var start = str[i].indexOf('[') + 1;
+    var dist = str[i].indexOf(']') - start;
     var idx = parseInt(str[i].substr(start, dist), 10);
     if(start !== 0)   str[i] = str[i].substr(0, start-1);
     return isNaN(idx) ? -1 : idx;
@@ -92,5 +114,5 @@ function getIndex(str, i = 0){
 
 function isElement(o){
     return (typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
-        o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName==="string");
+    o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName==="string");
 }
